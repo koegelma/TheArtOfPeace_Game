26f86d6d58785d3e80c34cbe8c04eac94e7eeed5
JohannSchulenburg <johann.schulenburg@hs-furtwangen.de>
2021-06-09 18:35:34 +0200

    Hand and Controller Script Revamp
    - Moved Controller logic from Spellmanager to a new non-static class Controller
    - working hands with animation

---
+++ Assets/Scripts/Hands/Controller.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Controller : MonoBehaviour
+{
+    public Vector3 controllerPosition;
+    public Quaternion controllerRotation;
+    public GameObject relativeTransform;
+    public InputDevice device;
+    public GameObject XRRig;
+    public Quaternion temporaryRotation;
+    public Vector3 controllerVelocity;
+    public Vector3 controllerAcceleration;
+    public GameObject mainCamera;
+    public float triggerValue;
+    public bool isTrigger;
+
+    void Update()
+    {
+        registerDevice();
+        updateRelativeControllerPositionAndRotation();
+    }
+    public void registerDevice()
+    {
+        if (Time.time % 1 <= 0.1)
+        {
+            var leftHanded = new List<UnityEngine.XR.InputDevice>();
+            var rightHanded = new List<UnityEngine.XR.InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Left, leftHanded);
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right, rightHanded);
+            if (this.gameObject.name.Equals("LeftController"))
+            {
+                foreach (var devices in leftHanded)
+                {
+                    this.device = devices;
+                }
+            }
+            else if (this.gameObject.name.Equals("RightController"))
+            {
+                foreach (var devices in rightHanded)
+                {
+                    this.device = devices;
+                }
+            }
+        }
+    }
+    public void updateRelativeControllerPositionAndRotation()
+    {
+        device.TryGetFeatureValue(CommonUsages.trigger, out triggerValue);
+        checkTrigger();
+        device.TryGetFeatureValue(CommonUsages.devicePosition, out controllerPosition);
+        device.TryGetFeatureValue(CommonUsages.deviceRotation, out controllerRotation);
+        device.TryGetFeatureValue(CommonUsages.deviceVelocity, out controllerVelocity);
+        device.TryGetFeatureValue(CommonUsages.deviceAcceleration, out controllerAcceleration);
+
+        controllerRotation *= Quaternion.Euler(Vector3.right * 20);
+
+        relativeTransform.transform.position = controllerPosition - mainCamera.transform.position;
+
+        Vector3 angles = controllerRotation.eulerAngles - mainCamera.transform.rotation.eulerAngles;
+        temporaryRotation = Quaternion.Euler(angles);
+
+        relativeTransform.transform.RotateAround(XRRig.transform.position, new Vector3(0, 1, 0), -mainCamera.transform.rotation.eulerAngles.y);
+        relativeTransform.transform.rotation = temporaryRotation;
+    }
+    void checkTrigger()
+    {
+        if (triggerValue >= 0.1)
+        {
+            isTrigger = true;
+        }
+        else
+        {
+            isTrigger = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Spells/Water.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public static List<Water> waterList;
+    public GameObject target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    Vector3 offset;
+    public void control(){
+        offset = target.transform.forward*3;
+        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position+offset, ref velocity, smoothTime);
+    }
+    void FixedUpdate()
+    {
+    }
+    void OnEnable(){
+        if(waterList == null)
+            waterList = new List<Water>();
+        waterList.Add(this);
+    }
+
+    void OnDisable(){
+        waterList.Remove(this);
+    }
+
+}
